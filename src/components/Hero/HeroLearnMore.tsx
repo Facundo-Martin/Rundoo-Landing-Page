26d2c32cce2cc3bfa0efc9c458abe11d17336e65
Facundo Martin <facundomartin98@gmail.com>
2022-09-19 15:22:44 -0700

    added RenderHero slide logic

---
+++ src/components/Hero/HeroLearnMore.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HeroLearnMore() {
+  return <div>HeroLearnMore</div>;
+}
+
+export default HeroLearnMore;
--- src/components/Hero/RenderHero.tsx
+++ src/components/Hero/RenderHero.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HeroLearnMore from './HeroLearnMore';
+import HeroMain from './HeroMain';
 
 function RenderHero() {
-  return <section className="wrapper section">RenderHero</section>;
+  const [isSigningUp, setIsSigningUp] = useState(false);
+  const slideHero = 'absolute inset-0 flex items-center justify-center transition-all ease-in-out duration-1000 transform';
+  return (
+    <div className="section wrapper relative w-full h-screen lg:h-[80vh]">
+      <div className={`${slideHero} ${isSigningUp ? 'translate-x-0' : '-translate-x-full'} `}>
+        <HeroLearnMore isSigningUp={isSigningUp} setIsSigningUp={setIsSigningUp} />
+      </div>
+      <div className={`${slideHero} ${isSigningUp ? 'translate-x-full' : 'translate-x-0'} `}>
+        <HeroMain isSigningUp={isSigningUp} setIsSigningUp={setIsSigningUp} />
+      </div>
+    </div>
+  );
 }
 
 export default RenderHero;
