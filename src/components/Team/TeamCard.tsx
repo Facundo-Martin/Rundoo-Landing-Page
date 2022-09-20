5f5f6be36f36424ee1fa736424d97660ae61ef68
Facundo Martin <facundomartin98@gmail.com>
2022-09-20 16:05:29 -0700

    finished adding TeamCard component and passed down some dummy data

--- src/components/Team/TeamCard.tsx
+++ src/components/Team/TeamCard.tsx
@@ -20,20 +20,20 @@ function TeamCard({ teammate }) {
   return (
     <div className="w-full border border-gray-200 rounded-lg shadow-sm">
       <div className="flex flex-col items-center justify-center p-10">
-        <img className="w-32 h-32 mb-6 rounded-full" src="https://cdn.devdojo.com/images/june2021/headshot2.jpg" />
-        <h2 className="text-lg font-medium">John Doe</h2>
-        <p className="font-medium text-blue-500">CEO and Founder</p>
-        <p className="text-gray-400">Team member as of 2015</p>
+        <img className="w-32 h-32 mb-6 rounded-full" src={teammate.avatar} />
+        <h2 className="text-lg font-medium">{teammate.name}</h2>
+        <p className="font-medium text-blue-500">{teammate.role}</p>
+        <p className="text-gray-400">Team member as of {teammate.since}</p>
       </div>
 
       <div className="flex border-t border-gray-200 divide-x divide-gray-200">
-        <SocialLink>
+        <SocialLink href={teammate.socials?.twitter ? teammate.socials.twitter : null}>
           <TwitterIcon />
         </SocialLink>
-        <SocialLink>
+        <SocialLink href={teammate.socials?.github ? teammate.socials.github : null}>
           <GithubIcon />
         </SocialLink>
-        <SocialLink>
+        <SocialLink href={teammate.socials?.linkedin ? teammate.socials.linkedin : null}>
           <LinkedInIcon />
         </SocialLink>
       </div>
