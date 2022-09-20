91367f9ed35f52c4d7aafe43c3e99bc39472b2f9
Facundo Martin <facundomartin98@gmail.com>
2022-09-20 16:49:56 -0700

    styled ContactDetails so it shows as side component

--- src/components/Contact/ContactDetails.tsx
+++ src/components/Contact/ContactDetails.tsx
@@ -5,7 +5,7 @@ export default function ContactDetails() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
-        <div className="mx-auto max-w-lg md:grid md:max-w-none md:grid-cols-2 md:gap-8">
+        <div className="mx-auto max-w-lg space-y-8">
           <div>
             <h2 className="text-2xl font-bold text-gray-900 sm:text-3xl sm:tracking-tight">Sales Support</h2>
             <div className="mt-3">
@@ -13,17 +13,17 @@ export default function ContactDetails() {
                 Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcu.
               </p>
             </div>
-            <div className="mt-9">
+            <div className="mt-9 flex flex-col lg:flex-row lg:items-center gap-8">
               <div className="flex">
                 <div className="flex-shrink-0">
                   <PhoneIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
                 </div>
                 <div className="ml-3 text-base text-gray-500">
                   <p>+1 (555) 123 4567</p>
-                  <p className="mt-1">Mon-Fri 8am to 6pm PST</p>
+                  {/* <p className="mt-1">Mon-Fri 8am to 6pm PST</p> */}
                 </div>
               </div>
-              <div className="mt-6 flex">
+              <div className="flex">
                 <div className="flex-shrink-0">
                   <EnvelopeIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
                 </div>
@@ -41,17 +41,17 @@ export default function ContactDetails() {
                 officiis facilis.
               </p>
             </div>
-            <div className="mt-9">
+            <div className="mt-9 flex flex-col lg:flex-row lg:items-center gap-8">
               <div className="flex">
                 <div className="flex-shrink-0">
                   <PhoneIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
                 </div>
                 <div className="ml-3 text-base text-gray-500">
                   <p>+1 (555) 123 4567</p>
-                  <p className="mt-1">Mon-Fri 8am to 6pm PST</p>
+                  {/* <p className="mt-1">Mon-Fri 8am to 6pm PST</p> */}
                 </div>
               </div>
-              <div className="mt-6 flex">
+              <div className="flex">
                 <div className="flex-shrink-0">
                   <EnvelopeIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
                 </div>
