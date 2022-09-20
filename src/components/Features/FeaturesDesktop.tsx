b0d443cb60af6919d259d8318958b4ce4020f61a
Facundo Martin <facundomartin98@gmail.com>
2022-09-20 15:25:02 -0700

    moved FeaturesDesktop to its own component

---
+++ src/components/Features/FeaturesDesktop.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image';
+import { Tab } from '@headlessui/react';
+import { Feature } from './RenderFeatures';
+import clsx from 'clsx';
+
+function FeaturesDesktop({ features }) {
+  return (
+    <Tab.Group as="div" className="hidden lg:mt-20 lg:block">
+      {({ selectedIndex }) => (
+        <>
+          <Tab.List className="grid grid-cols-3 gap-x-8">
+            {features.map((feature, featureIndex) => (
+              <Feature
+                key={feature.name}
+                feature={{
+                  ...feature,
+                  name: (
+                    <Tab className="[&:not(:focus-visible)]:focus:outline-none">
+                      <span className="absolute inset-0" />
+                      {feature.name}
+                    </Tab>
+                  ),
+                }}
+                isActive={featureIndex === selectedIndex}
+                className="relative"
+              />
+            ))}
+          </Tab.List>
+          <Tab.Panels className="relative mt-20 overflow-hidden rounded-4xl bg-slate-200 px-14 py-16 xl:px-16">
+            <div className="-mx-5 flex">
+              {features.map((feature, featureIndex) => (
+                <Tab.Panel
+                  static
+                  key={feature.name}
+                  className={clsx(
+                    'px-5 transition duration-500 ease-in-out [&:not(:focus-visible)]:focus:outline-none',
+                    featureIndex !== selectedIndex && 'opacity-60'
+                  )}
+                  style={{ transform: `translateX(-${selectedIndex * 100}%)` }}
+                  aria-hidden={featureIndex !== selectedIndex}
+                >
+                  <div className="w-[52.75rem] overflow-hidden rounded-xl bg-white shadow-lg shadow-slate-900/5 ring-1 ring-slate-500/10">
+                    <Image className="w-full" src={feature.image} alt="" sizes="52.75rem" />
+                  </div>
+                </Tab.Panel>
+              ))}
+            </div>
+            <div className="pointer-events-none absolute inset-0 rounded-4xl ring-1 ring-inset ring-slate-900/10" />
+          </Tab.Panels>
+        </>
+      )}
+    </Tab.Group>
+  );
+}
+
+export default FeaturesDesktop;
--- src/components/Features/RenderFeatures.tsx
+++ src/components/Features/RenderFeatures.tsx
@@ -6,6 +6,7 @@ import screenshotContacts from '../../images/contacts.webp';
 import screenshotInventory from '../../images/inventory.webp';
 import screenshotProfitLoss from '../../images/profit-loss.webp';
 import FeaturesMobile from './FeaturesMobile';
+import FeaturesDesktop from './FeaturesDesktop';
 
 const features = [
   {
@@ -83,56 +84,6 @@ export function Feature({ feature, isActive, className, ...props }: FeatureProps
   );
 }
 
-function FeaturesDesktop() {
-  return (
-    <Tab.Group as="div" className="hidden lg:mt-20 lg:block">
-      {({ selectedIndex }) => (
-        <>
-          <Tab.List className="grid grid-cols-3 gap-x-8">
-            {features.map((feature, featureIndex) => (
-              <Feature
-                key={feature.name}
-                feature={{
-                  ...feature,
-                  name: (
-                    <Tab className="[&:not(:focus-visible)]:focus:outline-none">
-                      <span className="absolute inset-0" />
-                      {feature.name}
-                    </Tab>
-                  ),
-                }}
-                isActive={featureIndex === selectedIndex}
-                className="relative"
-              />
-            ))}
-          </Tab.List>
-          <Tab.Panels className="relative mt-20 overflow-hidden rounded-4xl bg-slate-200 px-14 py-16 xl:px-16">
-            <div className="-mx-5 flex">
-              {features.map((feature, featureIndex) => (
-                <Tab.Panel
-                  static
-                  key={feature.name}
-                  className={clsx(
-                    'px-5 transition duration-500 ease-in-out [&:not(:focus-visible)]:focus:outline-none',
-                    featureIndex !== selectedIndex && 'opacity-60'
-                  )}
-                  style={{ transform: `translateX(-${selectedIndex * 100}%)` }}
-                  aria-hidden={featureIndex !== selectedIndex}
-                >
-                  <div className="w-[52.75rem] overflow-hidden rounded-xl bg-white shadow-lg shadow-slate-900/5 ring-1 ring-slate-500/10">
-                    <Image className="w-full" src={feature.image} alt="" sizes="52.75rem" />
-                  </div>
-                </Tab.Panel>
-              ))}
-            </div>
-            <div className="pointer-events-none absolute inset-0 rounded-4xl ring-1 ring-inset ring-slate-900/10" />
-          </Tab.Panels>
-        </>
-      )}
-    </Tab.Group>
-  );
-}
-
 function RenderFeatures() {
   return (
     <section id="features" aria-label="Features for simplifying everyday business tasks" className="section wrapper">
@@ -144,7 +95,7 @@ function RenderFeatures() {
           </p>
         </div>
         <FeaturesMobile features={features} />
-        <FeaturesDesktop />
+        <FeaturesDesktop features={features} />
       </div>
     </section>
   );
