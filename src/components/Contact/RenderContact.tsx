96a25a5151a404609d2a50b116216aebe6bbe402
Facundo Martin <facundomartin98@gmail.com>
2022-09-20 17:51:50 -0700

    finished adding Google Map API

--- src/components/Contact/RenderContact.tsx
+++ src/components/Contact/RenderContact.tsx
@@ -5,7 +5,7 @@ import ContactLocation from './ContactLocation';
 function RenderContact() {
   return (
     <section id="contact" className="wrapper section">
-      <div className="flex flex-col gap-6 lg:flex-row items-center justify-between lg:gap-x-8">
+      <div className="flex flex-col gap-6 xl:flex-row items-center justify-between lg:gap-x-8">
         <ContactLocation />
         <ContactDetails />
       </div>
