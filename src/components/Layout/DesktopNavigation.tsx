3520b8123545d7de7f4f2267cf81e76a2d34d02b
Facundo Martin <facundomartin98@gmail.com>
2022-09-21 08:29:32 -0700

    finished adding new Header (still not convinced about btn)

--- src/components/Layout/DesktopNavigation.tsx
+++ src/components/Layout/DesktopNavigation.tsx
@@ -3,13 +3,13 @@ import NavLink from '../UI/NavLink';
 
 function DesktopNavigation() {
   return (
-    <nav className="hidden md:flex space-x-6 lg:space-x-10  font-medium">
+    <div className="hidden md:flex space-x-6 lg:space-x-8  font-medium">
       <NavLink href="#features">Features</NavLink>
       <NavLink href="#team">Team</NavLink>
       <NavLink href="#FAQs">FAQs</NavLink>
       <NavLink href="#contact">Contact</NavLink>
       <span className="w-1 border-l border-gray-200 h-5 md:ml-6 lg:ml-10"></span>
-    </nav>
+    </div>
   );
 }
 
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,6 +2,7 @@ import NavLink from '../UI/NavLink';
 import MobileNavigation from './MobileNavigation';
 import DesktopNavigation from './DesktopNavigation';
 import Logo from '../UI/Logo';
+import Link from 'next/link';
 
 export default function Header() {
   return (
@@ -9,18 +10,17 @@ export default function Header() {
       <nav className="relative z-50 flex justify-between">
         <div className="w-full flex items-center justify-between">
           <Logo />
-
           <DesktopNavigation />
         </div>
-        <div className="flex items-center gap-x-5 md:gap-x-8">
-          <div className="hidden md:block">
+        <div className="flex items-center gap-x-5 md:gap-x-8 flex-none ml-6">
+          <div className="hidden md:block font-medium">
             <NavLink href="/login">Sign in</NavLink>
           </div>
-          <button color="blue">
-            <span>
+          <Link href="/get-started">
+            <a className="px-4 py-3 rounded-xl bg-blue-600 text-white font-semibold text-[.9rem]">
               Get started <span className="hidden lg:inline">today</span>
-            </span>
-          </button>
+            </a>
+          </Link>
           <div className="-mr-1 md:hidden">
             <MobileNavigation />
           </div>
