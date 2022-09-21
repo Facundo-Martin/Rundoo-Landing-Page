c90b7c51f0e5446b94291b6cb35b6a1db7c28598
Facundo Martin <facundomartin98@gmail.com>
2022-09-21 08:48:17 -0700

    fixed RenderHero general styling

--- src/components/Hero/RenderHero.tsx
+++ src/components/Hero/RenderHero.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React, { useState } from 'react';
 import HeroLearnMore from './HeroLearnMore';
 import HeroMain from './HeroMain';
@@ -7,17 +8,17 @@ function RenderHero() {
   const [showMore, setShowMore] = useState(false);
   const slideHero = 'absolute inset-0 flex items-center justify-center transition-all ease-in-out duration-1000 transform';
   return (
-    <div className="max-w-screen-xl mx-auto">
-      <div className="overflow-hidden relative w-full h-screen lg:h-[80vh]">
-        <div className={`${slideHero} ${showMore ? 'translate-x-0' : '-translate-x-full'} `}>
+    <section className="section">
+      <div className="overflow-hidden relative w-full h-[80vh] border-2">
+        <div className={clsx(slideHero, showMore ? 'translate-x-0' : '-translate-x-full')}>
           <HeroLearnMore setShowMore={setShowMore} />
         </div>
-        <div className={`${slideHero} ${showMore ? 'translate-x-full' : 'translate-x-0'} `}>
+        <div className={clsx(slideHero, showMore ? 'translate-x-full' : 'translate-x-0')}>
           <HeroMain setShowMore={setShowMore} />
         </div>
       </div>
       <HeroSponsors />
-    </div>
+    </section>
   );
 }
 
