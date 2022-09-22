d9e324799dc2e9aa32649b08a090afdc67b0a22d
Facundo Martin <facundomartin98@gmail.com>
2022-09-22 16:31:01 -0700

    added sponsors title

--- src/components/Hero/HeroSponsors.tsx
+++ src/components/Hero/HeroSponsors.tsx
@@ -8,31 +8,30 @@ import vulcan from '../../images/Sponsors/vulcan.png';
 
 function HeroSponsors() {
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-3">
-          <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <Image src={eaglematerials} alt="" width={200} height={200} />
+    <div className="section">
+      <h4 className="text-center text-2xl text-gray-500/90 font-medium mb-4 ">We work with</h4>
+      <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-3">
+        <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+          <Image src={eaglematerials} alt="" width={200} height={200} />
+        </div>
+        <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+          <div className="w-60 h-20">
+            <Image src={martinmarrieta} alt="" width={200} height={200} layout="responsive" />
           </div>
-          <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <div className="w-60 h-20">
-              <Image src={martinmarrieta} alt="" width={200} height={200} layout="responsive" />
-            </div>
+        </div>
+        <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+          <div className="w-32">
+            <Image src={quanta} alt="" width={200} height={200} layout="responsive" />
           </div>
-          <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <div className="w-32">
-              <Image src={quanta} alt="" width={200} height={200} layout="responsive" />
-            </div>
+        </div>
+        <div className="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
+          <div className="w-20">
+            <Image src={quanta} alt="" width={200} height={200} layout="responsive" />
           </div>
-          <div className="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
-            <div className="w-20">
-              <Image src={quanta} alt="" width={200} height={200} layout="responsive" />
-            </div>
-          </div>
-          <div className="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
-            <div className="w-20">
-              <Image src={quanta} alt="" width={200} height={200} layout="responsive" />
-            </div>
+        </div>
+        <div className="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+          <div className="w-20">
+            <Image src={quanta} alt="" width={200} height={200} layout="responsive" />
           </div>
         </div>
       </div>
