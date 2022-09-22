2029049577d6bef0177a681c4236bc49309f3770
Facundo Martin <facundomartin98@gmail.com>
2022-09-22 16:27:48 -0700

    gave up on stupid sponsors

--- src/components/Hero/HeroSponsors.tsx
+++ src/components/Hero/HeroSponsors.tsx
@@ -1,24 +1,38 @@
+import Image from 'next/image';
 import React from 'react';
+import eaglematerials from '../../images/Sponsors/eaglematerials.png';
+import granite from '../../images/Sponsors/granite.png';
+import martinmarrieta from '../../images/Sponsors/martinmarrieta.png';
+import quanta from '../../images/Sponsors/quanta.webp';
+import vulcan from '../../images/Sponsors/vulcan.png';
 
 function HeroSponsors() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
+        <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-3">
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img className="h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple" />
+            <Image src={eaglematerials} alt="" width={200} height={200} />
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img className="h-12" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage" />
+            <div className="w-60 h-20">
+              <Image src={martinmarrieta} alt="" width={200} height={200} layout="responsive" />
+            </div>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img className="h-12" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit" />
+            <div className="w-32">
+              <Image src={quanta} alt="" width={200} height={200} layout="responsive" />
+            </div>
           </div>
           <div className="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
-            <img className="h-12" src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg" alt="Transistor" />
+            <div className="w-20">
+              <Image src={quanta} alt="" width={200} height={200} layout="responsive" />
+            </div>
           </div>
           <div className="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
-            <img className="h-12" src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg" alt="Workcation" />
+            <div className="w-20">
+              <Image src={quanta} alt="" width={200} height={200} layout="responsive" />
+            </div>
           </div>
         </div>
       </div>
