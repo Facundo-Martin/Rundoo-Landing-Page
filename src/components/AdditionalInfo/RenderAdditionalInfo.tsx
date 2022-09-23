e9a3b592aacf5b171d263ed7fb9516e963376c93
Facundo Martin <facundomartin98@gmail.com>
2022-09-23 15:35:28 -0700

    deleted unused RenderAdditionalInfo component

--- src/components/AdditionalInfo/RenderAdditionalInfo.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function RenderAdditionalInfo() {
-  return <section className="wrapper section">RenderAdditionalInfo</section>;
-}
-
-export default RenderAdditionalInfo;
