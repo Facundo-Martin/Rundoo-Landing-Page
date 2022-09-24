61bd4ffba2094bbdefc4b2fd6686df0a4c9ff607
Facundo-Martin <facundomartin98@gmail.com>
2022-09-24 16:37:05 -0700

    finished adding team avatars

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,6 +1,19 @@
+import { StaticImageData } from 'next/image';
+import AndrewBeckman from '../images/Team/AndrewBeckman.jpeg';
+import DennisWanjiru from '../images/Team/DennisWanjiru.jpeg';
+import GeorgeYounger from '../images/Team/GeorgeYounger.jpeg';
+import JoeBosetti from '../images/Team/JoeBosetti.jpeg';
+import KelvinKariuki from '../images/Team/KelvinKariuki.jpeg';
+import NahomMarie from '../images/Team/NahomMarie.jpeg';
+import NickHershey from '../images/Team/NickHershey.jpeg';
+import SantiagoAguilar from '../images/Team/SantiagoAguilar.jpeg';
+import ShlokaReddy from '../images/Team/ShlokaReddy.jpeg';
+import UmerSiddiqui from '../images/Team/UmerSiddiqui.jpeg';
+import ZacharyIsaac from '../images/Team/ZacharyIsaac.jpeg';
+
 export interface Person {
   name: string;
-  avatar: string;
+  avatar: StaticImageData;
   role: string;
   since: number;
   socials: { twitter?: string; github?: string; linkedin: string };
@@ -8,77 +21,77 @@ export interface Person {
 export const team: Person[] = [
   {
     name: 'Nick Hershey',
-    avatar: '',
+    avatar: NickHershey,
     role: 'Co-Founder & CEO',
     since: 2021,
     socials: { twitter: '', github: '', linkedin: 'https://www.linkedin.com/in/hershey-nick/' },
   },
   {
     name: 'Andrew Beckman',
-    avatar: '',
+    avatar: AndrewBeckman,
     role: 'Co-Founder & CTO',
     since: 2021,
     socials: { twitter: '', github: '', linkedin: 'https://www.linkedin.com/in/beckman-andrew/' },
   },
   {
     name: 'George Younger',
-    avatar: '',
+    avatar: GeorgeYounger,
     role: 'Software Engineer',
     since: 2021,
     socials: { twitter: '', github: '', linkedin: 'https://www.linkedin.com/in/george-younger/' },
   },
   {
     name: 'Zachary Isaac',
-    avatar: '',
+    avatar: ZacharyIsaac,
     role: 'Product Designer',
     since: 2021,
     socials: { twitter: '', github: '', linkedin: 'https://www.linkedin.com/in/zachisaac/' },
   },
   {
     name: 'Joe Bosetti',
-    avatar: '',
+    avatar: JoeBosetti,
     role: 'Client Success Lead',
     since: 2022,
     socials: { twitter: '', github: '', linkedin: 'https://www.linkedin.com/in/josephbosetti1869a/' },
   },
   {
     name: 'Kelvin Kariuki',
-    avatar: '',
+    avatar: KelvinKariuki,
     role: 'Software Engineer',
     since: 2022,
     socials: { twitter: '', github: '', linkedin: 'https://www.linkedin.com/in/dotnesh/' },
   },
   {
     name: 'Umer Siddiqui',
-    avatar: '',
+    avatar: UmerSiddiqui,
     role: 'Software Engineer',
     since: 2022,
     socials: { twitter: '', github: '', linkedin: 'https://www.linkedin.com/in/umerhsiddiqui/' },
   },
   {
     name: 'Dennis Wanjiru',
-    avatar: '',
+    avatar: DennisWanjiru,
     role: 'Software Engineer',
     since: 2022,
     socials: { twitter: '', github: '', linkedin: 'https://www.linkedin.com/in/denniswanjiru/' },
   },
   {
     name: 'Shloka Reddy',
-    avatar: '',
+    avatar: ShlokaReddy,
     role: 'Software Engineer',
     since: 2022,
     socials: { twitter: '', github: '', linkedin: 'https://www.linkedin.com/in/shloka-reddy/' },
   },
   {
     name: 'Nahom Marie',
-    avatar: '',
+    avatar: NahomMarie,
     role: 'Software Engineer',
     since: 2022,
     socials: { twitter: '', github: '', linkedin: 'https://www.linkedin.com/in/nahommarie/' },
   },
   {
     name: 'Santiago Aguilar',
-    avatar: '',
+    avatar: SantiagoAguilar,
     role: 'Software Engineer Intern',
     since: 2022,
     socials: { twitter: '', github: '', linkedin: 'https://www.linkedin.com/in/aguilar-santiago/' },
