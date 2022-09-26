a3c4b3640355d73c83f9f81dfe7edf1b725512de
Facundo Martin <facundomartin98@gmail.com>
2022-09-26 08:53:56 -0700

    halfway through footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,5 +1,8 @@
+import Image from 'next/image';
 import { SVGProps } from 'react';
 import Logo from '../UI/Logo';
+import AppStoreLogo from '../../images/AppStoreLogo.png';
+import AppStoreDownload from '../../images/AppStoreDownload.svg';
 
 /* This example requires Tailwind CSS v2.0+ */
 const navigation = {
@@ -28,19 +31,6 @@ const navigation = {
     { name: 'Terms', href: '#' },
   ],
   social: [
-    {
-      name: 'Facebook',
-      href: '#',
-      icon: (props: SVGProps<SVGSVGElement>) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
     {
       name: 'Instagram',
       href: '#',
@@ -78,13 +68,13 @@ const navigation = {
     },
     {
       name: 'Dribbble',
-      href: '#',
+      href: 'https://www.getrundoo.com/',
       icon: (props: SVGProps<SVGSVGElement>) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
           <path
-            fillRule="evenodd"
-            d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-            clipRule="evenodd"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
           />
         </svg>
       ),
@@ -105,7 +95,7 @@ export default function Footer() {
             <p className="text-base text-gray-500">Making the world a better place through constructing elegant hierarchies.</p>
             <div className="flex space-x-6">
               {navigation.social.map((item) => (
-                <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+                <a key={item.name} href={item.href} target="_blank" rel="no-referrer" className="text-gray-400 hover:text-gray-500">
                   <span className="sr-only">{item.name}</span>
                   <item.icon className="h-6 w-6" aria-hidden="true" />
                 </a>
@@ -167,17 +157,21 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <div className="mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8 border-t border-gray-200 pt-8 mt-12">
-          <div className="flex justify-center space-x-6 md:order-2">
-            {navigation.social.map((item) => (
-              <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
-                <span className="sr-only">{item.name}</span>
-                <item.icon className="h-6 w-6" aria-hidden="true" />
-              </a>
-            ))}
-          </div>
+        <div className="flex justify-end pr-10 mt-6">
+          <a href="https://apps.apple.com/us/app/rundoo/id1530734240" target="_blank" rel="no-referrer">
+            <Image src={AppStoreDownload} width={180} height={80} />
+          </a>
+        </div>
+        <div className="mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8 border-t border-gray-200 pt-6 mt-4">
+          <div className="flex justify-center space-x-6 md:order-2"></div>
           <div className="mt-8 md:order-1 md:mt-0">
-            <p className="text-center text-base text-gray-400">&copy; {new Date().getFullYear()} Facundo Martin for Rundoo</p>
+            <p className="text-center text-base text-gray-400">
+              &copy; {new Date().getFullYear()}{' '}
+              <a href="https://facundomartin.io" target="_blank" rel="no-referrer" className="font-medium underline">
+                Facundo Martin
+              </a>{' '}
+              for Rundoo
+            </p>
           </div>
         </div>
       </div>
