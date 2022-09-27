86dec473462fded2d411bbc5bbea1c771c3e54dd
Facundo Martin <facundomartin98@gmail.com>
2022-09-27 08:13:48 -0700

    added modal close button

--- src/components/ComingSoonModal.tsx
+++ src/components/ComingSoonModal.tsx
@@ -1,5 +1,6 @@
 import React, { JSXElementConstructor, useState } from 'react';
 import { Dialog } from '@headlessui/react';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 
 type ComingSoonModalProps = {
   button: JSX.Element;
@@ -21,8 +22,12 @@ function ComingSoonModal({ button }: ComingSoonModalProps) {
         {/* Full-screen container to center the panel */}
         <div className="fixed inset-0 flex items-center justify-center p-4">
           {/* The actual dialog panel  */}
-          <Dialog.Panel className="mx-auto w-full max-w-xl h-60 rounded bg-gray-300">
-            <Dialog.Title className="text-center my-2 text-3xl">Coming Soon!</Dialog.Title>
+          <Dialog.Panel className="mx-auto w-full max-w-xl h-60 rounded bg-stone-100 relative">
+            <Dialog.Title className="text-center my-3 text-3xl font-medium">Coming Soon!</Dialog.Title>
+            <div>etc </div>
+            <button className="absolute right-[6px] top-[6px] text-sm ">
+              <XMarkIcon className="w-5" onClick={() => setIsOpen(false)} />
+            </button>
 
             {/* ... */}
           </Dialog.Panel>
