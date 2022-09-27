7169b7a0166e5c74c979163ec126e47cc35b8872
Facundo Martin <facundomartin98@gmail.com>
2022-09-27 06:39:57 -0700

    added LinkedIn svg icon

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image';
 import { SVGProps } from 'react';
 import Logo from '../UI/Logo';
-import AppStoreLogo from '../../images/AppStoreLogo.png';
 import AppStoreDownload from '../../images/AppStoreDownload.svg';
-import clsx from 'clsx';
 
 /* This example requires Tailwind CSS v2.0+ */
 const navigation = {
@@ -63,15 +61,34 @@ const navigation = {
       ),
     },
     {
-      name: 'Dribbble',
-      href: 'https://www.getrundoo.com/',
+      name: 'LinkedIn',
+      href: 'https://www.linkedin.com/company/rundoo/',
       icon: (props: SVGProps<SVGSVGElement>) => (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
-          />
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="21" height="21" viewBox="0 0 192 192" className="mt-[1.5px]">
+          {
+            <g
+              fill="none"
+              fill-rule="nonzero"
+              stroke="none"
+              stroke-width="1"
+              stroke-linecap="butt"
+              stroke-linejoin="miter"
+              stroke-miterlimit="10"
+              stroke-dasharray=""
+              stroke-dashoffset="0"
+              font-family="none"
+              font-weight="none"
+              font-size="none"
+              text-anchor="none"
+            >
+              <path d="M0,192v-192h192v192z" fill="none"></path>
+              <g fill="currentColor">
+                <g id="surface1">
+                  <path d="M156,0h-120c-19.875,0 -36,16.125 -36,36v120c0,19.875 16.125,36 36,36h120c19.875,0 36,-16.125 36,-36v-120c0,-19.875 -16.125,-36 -36,-36zM59.36539,162.98077h-29.82693l-0.17307,-89.30769h29.82692zM43.70192,61.99038h-0.17308c-9.75,0 -16.03846,-6.72115 -16.03846,-15.08653c0,-8.56731 6.49039,-15.0577 16.41347,-15.0577c9.92308,0 16.00961,6.49038 16.21153,15.0577c0,8.36538 -6.31731,15.08653 -16.41346,15.08653zM162.77885,162.98077h-30.08654v-48.51923c0,-11.74039 -3.11538,-19.73077 -13.61538,-19.73077c-8.01923,0 -12.34615,5.39423 -14.42308,10.61538c-0.77885,1.875 -0.98077,4.44231 -0.98077,7.06731v50.56731h-30.23077l-0.17308,-89.30769h30.23077l0.17308,12.60577c3.86538,-5.97116 10.29808,-14.42308 25.70192,-14.42308c19.09616,0 33.37501,12.46154 33.37501,39.25961v51.86539z"></path>
+                </g>
+              </g>
+            </g>
+          }
         </svg>
       ),
     },
