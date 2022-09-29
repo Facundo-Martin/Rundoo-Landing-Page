d5191a2811d32c58eba1218ca9b3121c83020f9d
Facundo Martin <facundomartin98@gmail.com>
2022-09-29 09:12:01 -0700

    added Feature type definition and solid icons on hover for LearnMore component

--- src/components/Hero/HeroLearnMore.tsx
+++ src/components/Hero/HeroLearnMore.tsx
@@ -1,28 +1,45 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { ArrowSmallLeftIcon, BoltIcon, ChatBubbleBottomCenterTextIcon, GlobeAltIcon, ScaleIcon } from '@heroicons/react/24/outline';
+import { ArrowSmallLeftIcon, ShoppingCartIcon, Square3Stack3DIcon, TruckIcon, UserGroupIcon } from '@heroicons/react/24/outline';
+import {
+  ShoppingCartIcon as SolidShoppingCartIcon,
+  Square3Stack3DIcon as SolidSquare3Stack3DIcon,
+  TruckIcon as SolidTruckIcon,
+  UserGroupIcon as SolidUserGroupIcon,
+} from '@heroicons/react/24/solid';
+import { SVGProps } from 'react';
 
-const features = [
+type Feature = {
+  name: string;
+  description: string;
+  icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  hoverIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+};
+const features: Feature[] = [
   {
     name: 'Wide range of materials',
     description:
       'We work with a large amount of suppliers to make sure our marketplace has everything you need for your job site. If you need it, Rundoo has it! ',
-    icon: GlobeAltIcon,
+    icon: Square3Stack3DIcon,
+    hoverIcon: SolidSquare3Stack3DIcon,
   },
   {
     name: 'Fast checkout process',
     description: 'Buying supplies has never been easier. Within a couple of clicks you can select your supplier, materials, amount and checkout!',
-    icon: ScaleIcon,
+    icon: ShoppingCartIcon,
+    hoverIcon: SolidShoppingCartIcon,
   },
   {
     name: 'Schedule pickup & delivery',
     description:
       'Flexibility is one of the most important aspects of building. You can order immediately or schedule for later. You can choose to pick up your materials, or get them delivered.',
-    icon: BoltIcon,
+    icon: TruckIcon,
+    hoverIcon: SolidTruckIcon,
   },
   {
     name: 'Order tracking & status',
     description: 'Stay up to date with your order. Rundoo allows your whole team to track the order and be notified of any changes to the order status.',
-    icon: ChatBubbleBottomCenterTextIcon,
+    icon: UserGroupIcon,
+    hoverIcon: SolidUserGroupIcon,
   },
 ];
 
@@ -42,14 +59,14 @@ export default function Example({ setShowMore }: { setShowMore: (showMore: boole
           Rundoo is designed for both contractors and suppliers, with the purpose of making construction more affordable and safer.
         </p>
       </div>
-
       <div className="mt-10">
         <dl className="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 md:space-y-0">
           {features.map((feature) => (
-            <div key={feature.name} className="relative">
+            <div key={feature.name} className="relative group">
               <dt>
                 <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
-                  <feature.icon className="h-6 w-6" aria-hidden="true" />
+                  <feature.icon className="h-6 w-6 group-hover:hidden" aria-hidden="true" />
+                  <feature.hoverIcon className="h-6 w-6 hidden group-hover:block" aria-hidden="true" />
                 </div>
                 <p className="ml-16 text-lg font-medium leading-6 text-gray-900">{feature.name}</p>
               </dt>
