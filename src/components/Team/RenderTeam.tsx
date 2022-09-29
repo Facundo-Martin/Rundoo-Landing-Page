c88399236c8ba66212c7c1198abec097bab417b2
Facundo Martin <facundomartin98@gmail.com>
2022-09-29 13:56:04 -0700

    added ContactMap address styling

--- src/components/Team/RenderTeam.tsx
+++ src/components/Team/RenderTeam.tsx
@@ -10,7 +10,7 @@ function RenderTeam() {
           <div className="max-w-3xl mb-20 space-y-5 sm:mx-auto sm:space-y-4">
             <h2 className="relative text-4xl font-extrabold tracking-tight sm:text-5xl">Our Awesome Team</h2>
             <p className="text-xl text-gray-500">
-              We have a diverse team of exceptional people working together to provide world-class service in the construction and building materials industry
+              We have a diverse team of exceptional people working together to provide world-class services in the construction and building materials industry
             </p>
           </div>
         </div>
