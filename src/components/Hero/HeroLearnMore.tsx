b0b4830b244f31962fbed05ca265350ef4c21936
Facundo Martin <facundomartin98@gmail.com>
2022-09-29 07:05:56 -0700

    changed main heros style

--- src/components/Hero/HeroLearnMore.tsx
+++ src/components/Hero/HeroLearnMore.tsx
@@ -1,71 +1,68 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { ArrowSmallLeftIcon, BoltIcon, GlobeAltIcon, ScaleIcon } from '@heroicons/react/24/outline';
+import { ArrowSmallLeftIcon, BoltIcon, ChatBubbleBottomCenterTextIcon, GlobeAltIcon, ScaleIcon } from '@heroicons/react/24/outline';
 
-const transferFeatures = [
+const features = [
   {
-    id: 1,
     name: 'Competitive exchange rates',
     description:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
     icon: GlobeAltIcon,
   },
   {
-    id: 2,
     name: 'No hidden fees',
     description:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
     icon: ScaleIcon,
   },
   {
-    id: 3,
     name: 'Transfers are instant',
     description:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
     icon: BoltIcon,
   },
+  {
+    name: 'Mobile notifications',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: ChatBubbleBottomCenterTextIcon,
+  },
 ];
 
-type HeroLearnMoreProps = {
-  setShowMore: (show: false) => void;
-};
-export default function HeroLearnMore({ setShowMore }: HeroLearnMoreProps) {
+export default function Example({ setShowMore }: { setShowMore: (showMore: boolean) => void }) {
   return (
-    <div className="overflow-hidden section bg-gray-50">
-      <div className="">
-        <button onClick={() => setShowMore(false)} className="">
-          <div className="flex items-center gap-2 group">
-            <ArrowSmallLeftIcon className="w-7 group-hover:-translate-x-[6px] transition-all ease-in-out duration-700 transform" />
-            <p className="text-lg ">Go Back</p>
-          </div>
-        </button>
-        <div className="relative mt-12 lg:grid lg:grid-cols-2 lg:items-center lg:gap-8">
-          <div className="relative">
-            <h3 className="hidden lg:block text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Transfer funds world-wide</h3>
-            <p className="hidden lg:block mt-3 text-lg text-gray-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima sequi recusandae, porro maiores officia assumenda aliquam laborum ab
-              aliquid veritatis impedit odit adipisci optio iste blanditiis facere. Totam, velit.
-            </p>
-
-            <dl className="mt-10 space-y-10">
-              {transferFeatures.map((item) => (
-                <div key={item.id} className="relative">
-                  <dt>
-                    <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
-                      <item.icon className="h-6 w-6" aria-hidden="true" />
-                    </div>
-                    <p className="ml-16 text-lg font-medium leading-6 text-gray-900">{item.name}</p>
-                  </dt>
-                  <dd className="mt-2 ml-16 text-base text-gray-500">{item.description}</dd>
-                </div>
-              ))}
-            </dl>
-          </div>
-
-          <div className="relative -mx-4 mt-10 lg:mt-0" aria-hidden="true">
-            <img className="relative mx-auto" width={490} src="https://tailwindui.com/img/features/feature-example-1.png" alt="" />
-          </div>
+    <div className="section bg-slate-50/70 py-12">
+      <button onClick={() => setShowMore(false)} className="mb-4">
+        <div className="flex items-center gap-2 group">
+          <ArrowSmallLeftIcon className="w-7 group-hover:-translate-x-[6px] transition-all ease-in-out duration-700 transform" />
+          <p className="text-lg ">Go Back</p>
         </div>
+      </button>
+      <div className="lg:text-center">
+        <h2 className="text-lg font-semibold text-indigo-600">Rundoo Marketplace</h2>
+        <p className="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">A better way to buy materials</p>
+        <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+          Rundoo is designed for both contractors and suppliers, with the purpose of making construction more affordable and safer.
+        </p>
+      </div>
+
+      <div className="mt-10">
+        <dl className="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 md:space-y-0">
+          {features.map((feature) => (
+            <div key={feature.name} className="relative">
+              <dt>
+                <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                  <feature.icon className="h-6 w-6" aria-hidden="true" />
+                </div>
+                <p className="ml-16 text-lg font-medium leading-6 text-gray-900">{feature.name}</p>
+              </dt>
+              <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
+            </div>
+          ))}
+        </dl>
       </div>
     </div>
   );
 }
+
+//    <div className="overflow-hidden section bg-gray-50">
+// <div className="">
--- src/components/Hero/HeroMain.tsx
+++ src/components/Hero/HeroMain.tsx
@@ -16,7 +16,7 @@ function HeroMain({ setShowMore }: HeroMainProps) {
         <div className="mx-auto sm:px-4 xl:px-0 flex items-center lg:flex-row flex-col h-full">
           <div className="w-full relative lg:w-1/2 flex-shrink-0 h-full flex flex-col xl:pr-0 lg:pr-5 lg:items-start sm:items-center justify-center space-y-10">
             <h1 className="font-extrabold text-4xl sm:text-5xl xl:text-6xl sm:text-center lg:text-left relative tracking-tight">
-              Start Crafting Your<br></br>Next Great Idea
+              Start Ordering Your<br></br>Supplies Online
             </h1>
 
             <div className="flex max-w-sm items-center lg:max-w-lg">
@@ -26,8 +26,8 @@ function HeroMain({ setShowMore }: HeroMainProps) {
                 <img src="https://cdn.devdojo.com/tails/avatars/041.jpg" className="rounded-full -ml-5 w-12 h-12 border-2 border-white" />
               </div>
               <p className="pl-3 lg:text-base text-xs">
-                Join <span className="font-bold">23,000</span> other developers as we build the next greatest <span className="font-bold">SAAS tools</span>{' '}
-                together!
+                Join <span className="font-bold">23,000</span> other contractors and start ordering <span className="font-bold">construction supplies</span>{' '}
+                online!
               </p>
             </div>
 
@@ -58,17 +58,9 @@ function HeroMain({ setShowMore }: HeroMainProps) {
 
           <div className="max-w-xl lg:w-1/2 flex-shrink-0 h-full lg:pt-0 pt-24">
             <div className="relative h-full w-full pl-12 lg:pr-0 pr-10">
-              <div className="h-auto transform xl:translate-x-8 translate-y-2 w-32 sm:w-56 xl:w-64  absolute border border-gray-100 left-0 bottom-0 z-10 rounded-lg shadow-2xl overflow-hidden p-2 bg-white">
-                <img src="https://cdn.devdojo.com/images/january2022/chart-1.png" className="" />
-              </div>
-              <div className="h-auto transform translate-x-5 lg:translate-x-6 -translate-y-5 xl:-translate-y-8 w-32 sm:w-56 xl:w-64 absolute border border-gray-100 right-0 z-10 top-0 rounded-xl shadow-2xl overflow-hidden p-2 bg-white">
-                <img src="https://cdn.devdojo.com/images/january2022/chart-2.png" />
-              </div>
-              <div className="relative flex items-center justify-center p-3">
-                <div className="bg-gray-100 transform rounded-xl p-2 shadow-2xl border border-gray-100 overflow-hidden h-auto w-full flex flex-col">
-                  <img src="https://cdn.devdojo.com/images/january2022/admin-bg.png" className="h-auto opacity-100" />
-                </div>
-              </div>
+              {/* <div>
+                <Image src="" width={100} height={200} alt="" />
+              </div> */}
             </div>
           </div>
         </div>
