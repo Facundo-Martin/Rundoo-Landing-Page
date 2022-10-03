64058dcd34f1673c1457140bc4a3715c38471a8d
Facundo Martin <facundomartin98@gmail.com>
2022-10-03 06:45:03 -0700

    fixed features button

--- src/components/Features/FeaturesDesktop.tsx
+++ src/components/Features/FeaturesDesktop.tsx
@@ -15,40 +15,32 @@ function FeaturesDesktop({ features }: FeatureProps) {
   const imageStyle = 'rounded-xl -mx-12';
   console.log('isActive', isActive);
   return (
-    <div className="flex mt-20 justify-between  items-center gap-x-10">
+    <div className="flex mt-20 justify-between  items-center gap-x-14">
       <div className="flex flex-col ">
         {features.map((feature, featureIndex) => (
-          <button className="max-w-md text-left" key={featureIndex} onClick={() => setIsActive(featureIndex)}>
+          <button className="relative max-w-md text-left" onClick={() => setIsActive(featureIndex)}>
             <Feature
               feature={{
                 ...feature,
-                name: (
-                  <div className="[&:not(:focus-visible)]:focus:outline-none">
-                    <span className="absolute inset-0" />
-                    {feature.name}
-                  </div>
-                ),
+                name: <div>{feature.name}</div>,
               }}
-              className={clsx(
+              className={
                 isActive == featureIndex ? 'bg-neutral-800/10 transform duration-300 opacity-100' : 'hover:opacity-100 hover:bg-neutral-800/5 duration-500'
-              )}
+              }
             />
           </button>
         ))}
       </div>
       <div className="relative">
-        <div
-          className={clsx('px-5 transition duration-500 ease-in-out [&:not(:focus-visible)]:focus:outline-none flex')}
-          // style={{ transform: `translateY(-${selectedIndex * 100}%)` }}
-        >
+        <div className="px-5 flex">
           <div className={clsx(isActive == 0 ? bringForward : 'scale-100 duration-700', imageStyle)}>
-            <Image src={RundooBrowseSupplies} alt="" height={550} width={280} />
+            <Image src={RundooBrowseSupplies} alt="" height={520} width={320} />
           </div>
           <div className={clsx(isActive == 1 ? bringForward : 'scale-100 duration-700', imageStyle, 'z-10')}>
-            <Image src={RundooBrowseSupplies} alt="" height={550} width={280} />
+            <Image src={RundooBrowseSupplies} alt="" height={520} width={320} />
           </div>
           <div className={clsx(isActive == 2 ? bringForward : 'scale-100 duration-700', imageStyle)}>
-            <Image src={RundooBrowseSupplies} alt="" height={550} width={280} />
+            <Image src={RundooBrowseSupplies} alt="" height={520} width={320} />
           </div>
         </div>
       </div>
