421f78d68fef66e504e2e0993fe7e289c84045d6
Facundo Martin <facundomartin98@gmail.com>
2022-10-03 09:05:26 -0700

    ScrollTopButton clean up

--- src/components/ScrollTopButton.tsx
+++ src/components/ScrollTopButton.tsx
@@ -1,19 +1,6 @@
 import { ArrowUpIcon } from '@heroicons/react/24/outline';
-import { useState } from 'react';
 
 const ScrollTopButton = () => {
-  const [visible, setVisible] = useState(false);
-
-  const toggleVisible = () => {
-    const scrolled = document.documentElement.scrollTop;
-    console.log('running scroll');
-    if (scrolled > 1400) {
-      setVisible(true);
-    } else if (scrolled <= 300) {
-      setVisible(false);
-    }
-  };
-
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -21,18 +8,14 @@ const ScrollTopButton = () => {
     });
   };
 
-  window.addEventListener('scroll', toggleVisible);
-
   return (
     <section className="wrapper w-full flex justify-end">
-      {visible && (
-        <button onClick={scrollToTop}>
-          <div className="flex flex-col items-center group">
-            <ArrowUpIcon className="w-5 group-hover:-translate-y-2 transition-all duration-700" />
-            <p> Back to Top</p>
-          </div>
-        </button>
-      )}
+      <button onClick={scrollToTop}>
+        <div className="flex flex-col items-center group">
+          <ArrowUpIcon className="w-5 group-hover:-translate-y-2 transition-all duration-700" />
+          <p> Back to Top</p>
+        </div>
+      </button>
     </section>
   );
 };
