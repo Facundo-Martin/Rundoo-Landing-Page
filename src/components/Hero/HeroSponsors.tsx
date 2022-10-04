aee90679a00981fbe2ff4c7f44b5fc8e7a22152a
Facundo Martin <facundomartin98@gmail.com>
2022-10-04 07:00:55 -0700

    fixed HeroSponsors images and added alt tags

--- src/components/Hero/HeroSponsors.tsx
+++ src/components/Hero/HeroSponsors.tsx
@@ -7,35 +7,24 @@ import quanta from '../../images/Sponsors/quanta.png';
 import vulcan from '../../images/Sponsors/vulcan.png';
 
 function HeroSponsors() {
-  const imageContainer = 'w-32';
   return (
     <div className="section">
       <h4 className="text-center text-2xl text-gray-500/90 font-medium mb-6">We work with</h4>
       <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
         <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-          <div className={imageContainer}>
-            <Image src={eaglematerials} alt="" width={180} height={80} layout="responsive" />
-          </div>
+          <Image src={eaglematerials} alt="Eagle Materials" width={140} height={50} />
         </div>
         <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-          <div className={imageContainer}>
-            <Image src={martinmarietta} alt="" width={180} height={80} layout="responsive" />
-          </div>
+          <Image src={martinmarietta} alt="Martin Marietta " width={140} height={50} />
         </div>
         <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-          <div className={imageContainer}>
-            <Image src={granite} alt="" width={180} height={80} layout="responsive" />
-          </div>
+          <Image src={granite} alt="Granite" width={140} height={50} />
         </div>
         <div className="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
-          <div className={imageContainer}>
-            <Image src={quanta} alt="" width={180} height={80} layout="responsive" />
-          </div>
+          <Image src={quanta} alt="Quanta services" width={140} height={50} />
         </div>
         <div className="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
-          <div className={imageContainer}>
-            <Image src={vulcan} alt="" width={180} height={80} layout="responsive" />
-          </div>
+          <Image src={vulcan} alt="Vulcan Materials Company" width={140} height={50} />
         </div>
       </div>
     </div>
