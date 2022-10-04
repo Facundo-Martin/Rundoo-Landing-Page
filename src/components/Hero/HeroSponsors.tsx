8769b84d1260fe3775a1e8b4f1f2fa6c96aab253
Facundo Martin <facundomartin98@gmail.com>
2022-10-04 07:02:06 -0700

    HeroSponsors style fix

--- src/components/Hero/HeroSponsors.tsx
+++ src/components/Hero/HeroSponsors.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import React from 'react';
+// images
 import eaglematerials from '../../images/Sponsors/eaglematerials.png';
 import granite from '../../images/Sponsors/granite.png';
 import martinmarietta from '../../images/Sponsors/martinmarietta.png';
@@ -8,8 +8,8 @@ import vulcan from '../../images/Sponsors/vulcan.png';
 
 function HeroSponsors() {
   return (
-    <div className="section">
-      <h4 className="text-center text-2xl text-gray-500/90 font-medium mb-6">We work with</h4>
+    <section className="section">
+      <h4 className="text-center text-2xl text-gray-500/90 font-medium mb-8">We work with</h4>
       <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
         <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
           <Image src={eaglematerials} alt="Eagle Materials" width={140} height={50} />
@@ -27,7 +27,7 @@ function HeroSponsors() {
           <Image src={vulcan} alt="Vulcan Materials Company" width={140} height={50} />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
